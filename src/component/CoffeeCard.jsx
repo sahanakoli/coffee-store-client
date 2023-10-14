7248404bc397e18bafc4766c383f1318b8739103
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-15 00:04:55 +0600

    delete a coffee and fetch coffee to update

--- src/component/CoffeeCard.jsx
+++ src/component/CoffeeCard.jsx
@@ -1,30 +1,70 @@
 /* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
 
 
-const CoffeeCard = ({coffee}) => {
+const CoffeeCard = ({ coffee }) => {
 
-    const {name, quantity, supplier, taste, photo} = coffee
+    const { _id, name, quantity, supplier, taste, photo } = coffee
 
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your Coffee has been deleted.',
+                    'success'
+                  )
+
+                fetch(`http://localhost:5000/coffee/${_id}`,{
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        // if (data.deleteCount > 0) {
+                        //       Swal.fire(
+                        //         'Deleted!',
+                        //         'Your Coffee has been deleted.',
+                        //         'success'
+                        //       )
+                        // }
+                    })
+                console.log('delete confirmed')
+            }
+        })
+    }
     return (
         <div>
             <div className="card w-10/12 h-48 mt-10 card-side bg-base-100 shadow-xl">
-            <figure><img src={photo} alt="Movie" /></figure>
-            <div className="flex justify-between w-full pr-4">
-                <div>
-                    <h2 className="card-title">Name: {name}</h2>
-                    <p>{quantity}</p>
-                    <p>{supplier}</p>
-                    <p>{taste}</p>
-                </div>
-                <div className="card-actions justify-end">
-                    <div className="btn-group btn-group-vertical space-y-4">
-                        <button className="btn">View</button>
-                        <button className="btn">Edit</button>
-                        <button className="btn">X</button>
+                <figure><img src={photo} alt="Movie" /></figure>
+                <div className="flex justify-between w-full pr-4">
+                    <div>
+                        <h2 className="card-title">Name: {name}</h2>
+                        <p>{quantity}</p>
+                        <p>{supplier}</p>
+                        <p>{taste}</p>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <div className="btn-group btn-group-vertical space-y-4">
+                            <button className="btn">View</button>
+                            <Link to={`updateCoffee/${_id}`}><button className="btn">Edit</button></Link>
+                            <button
+                                onClick={() => handleDelete(_id)}
+                                className="btn bg-red-400">X</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 };
