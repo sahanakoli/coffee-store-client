e88009f27376da17ffb7134b6a6a1ee340c1ce26
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-17 22:30:10 +0600

    reload error solved

--- src/firebase/firebase.confiq.js
+++ src/firebase/firebase.confiq.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQRZXcOTkffP4DK4UylIL57LL-C_cDf6E",
-  authDomain: "coffee-store-b44d9.firebaseapp.com",
-  projectId: "coffee-store-b44d9",
-  storageBucket: "coffee-store-b44d9.appspot.com",
-  messagingSenderId: "721877551865",
-  appId: "1:721877551865:web:312c26df0ef4c366eab028",
-  measurementId: "G-NM44TDRX99"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTId
 };
 
 // Initialize Firebase
